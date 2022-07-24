582891966559562a3c9765a4ddb649fedae461e9
Sabah <67190764+DBCooper200@users.noreply.github.com>
2022-07-24 16:46:07 +0530

    dependencies for login api

---
+++ src/main/java/com/bnmbackend/bnm_backend/BnmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bnmbackend.bnm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnmbackend/bnm_backend/BnmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnmbackend.bnm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
